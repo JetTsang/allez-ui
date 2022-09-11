435c695b4c1efd64154303c0590b37dfc7580a95
JetTsang <1109126828@qq.com>
2022-09-11 12:19:44 +0800

    feat: docs加入md插槽

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,14 @@
 import Theme from 'vitepress/dist/client/theme-default'
 import AllezUI from '../../../src/entry'
-
+import 'vitepress-theme-demoblock/theme/styles/index.css'
+// 插件的组件，主要是demo组件
+import Demo from 'vitepress-theme-demoblock/components/Demo.vue'
+import DemoBlock from 'vitepress-theme-demoblock/components/DemoBlock.vue'
 export default {
   ...Theme,
   enhanceApp({ app }) { // 在这里注册组件库
     app.use(AllezUI)
+    app.component('Demo', Demo)
+    app.component('DemoBlock', DemoBlock)
   },
 }
--- docs/components/button/index.md
+++ docs/components/button/index.md
@@ -2,12 +2,16 @@
 常用的操作按钮。
 ## 基础用法
 基础的按钮用法。
- <div class="flex flex-row">
+ <!-- <div class="flex flex-row">
   <TSXbutton icon="edit" plain color="red">编辑</TSXbutton>
   <TSXbutton icon="delete" plain>删除</TSXbutton>
   <TSXbutton icon="share" plain>分享</TSXbutton>
   <TSXbutton round plain icon="search">搜索</TSXbutton>
- </div>
+ </div> -->
+ 
+ :::demo 使用`size`、`color`、`pain`、`round`属性来定义 Button 的样式。
+
+
 ```vue
 <template>
  <div class="flex flex-row">
@@ -18,3 +22,4 @@
  </div>
 </template>
 ```
+:::
